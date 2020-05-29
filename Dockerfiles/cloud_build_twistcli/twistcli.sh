92ffabedfd5ada18afaa16d7fb3472c7ac54d5f7
zohebsarwar82 <58395751+zohebsarwar82@users.noreply.github.com>
2020-05-30 07:54:15 +1000

    Update twistcli.sh

--- Dockerfiles/cloud_build_twistcli/twistcli.sh
+++ Dockerfiles/cloud_build_twistcli/twistcli.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-# curl -k -u $TL_USER:$TL_PASS --output /twistcli $TL_CONSOLE_URL/api/v1/util/twistcli
+#curl -k -u $TL_USER:$TL_PASS --output /twistcli $TL_CONSOLE_URL/api/v1/util/twistcli
 chmod a+x /twistcli
-/twistcli images scan --details -address $TL_CONSOLE_URL -u $TL_USER -p $TL_PASS --ci $1
+# /twistcli images scan --details -address $TL_CONSOLE_URL -u $TL_USER -p $TL_PASS --vulnerability-threshold $VULN_THRESHOLD --compliance-threshold $COMP_THRESHOLD $1
+/twistcli images scan --details -address $TL_CONSOLE_URL -u $TL_USER -p $TL_PASS $1
